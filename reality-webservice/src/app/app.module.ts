decd1c68d45997bfe4d0afd3dc44d07a775023d1
Thaifactory <michael.ratke@t-online.de>
2018-04-09 20:34:52 +0200

    First upload. Create Routing and basic style elements.

--- reality-webservice/src/app/app.module.ts
+++ reality-webservice/src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { LosnummerComponent } from './losnummer/losnummer.component';
+import { BewertungComponent } from './bewertung/bewertung.component';
+import { UnternehmenService } from "./_services/unternehmen.service";
+
+const routes: Routes = [
+  { path: 'losnummer', component: LosnummerComponent },
+  { path: 'bewertung', component: BewertungComponent },
+  { path: '**', component: LosnummerComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LosnummerComponent,
+    BewertungComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [UnternehmenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

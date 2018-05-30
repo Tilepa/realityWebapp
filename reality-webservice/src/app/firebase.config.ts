5103415b5abece95038a989da365ce99e64335bc
Thaifactory <michael.ratke@t-online.de>
2018-05-30 07:52:55 +0200

    Delete API Key

--- reality-webservice/src/app/firebase.config.ts
+++ reality-webservice/src/app/firebase.config.ts
@@ -1,5 +1,5 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBc2fw6juv-Q7DaWuJ1Cdkirp2Nvfh7a9g",
+  apiKey: "",
   authDomain: "realitywebapp.firebaseapp.com",
   databaseURL: "https://realitywebapp.firebaseio.com",
   projectId: "realitywebapp",

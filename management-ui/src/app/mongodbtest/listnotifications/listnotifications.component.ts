19d1cc6a579cdc5460282b68e51ac7b3611a7bbe
timonSachweh1997 <timon.sachweh@udo.edu>
2019-04-04 13:55:51 +0200

    Add sample view for mongodbtest

--- management-ui/src/app/mongodbtest/listnotifications/listnotifications.component.ts
+++ management-ui/src/app/mongodbtest/listnotifications/listnotifications.component.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listnotifications.component.scss']
 })
 export class ListnotificationsComponent implements OnInit {
+  private news: ({ id: string; title: string; content: string })[];
 
   constructor() { }
 
   ngOnInit() {
+    this.news = [
+      {
+        id: 'test1',
+        title: 'test1',
+        content: 'test1'
+      }, {
+        id: 'test2',
+        title: 'test2',
+        content: 'test2'
+      },
+      {
+        id: 'test1',
+        title: 'test1',
+        content: 'test1'
+      }, {
+        id: 'test2',
+        title: 'test2',
+        content: 'test2'
+      },
+      {
+        id: 'test1',
+        title: 'test1',
+        content: 'test1'
+      }, {
+        id: 'test2',
+        title: 'test2',
+        content: 'test2'
+      },
+      {
+        id: 'test1',
+        title: 'test1',
+        content: 'test1'
+      }, {
+        id: 'test2',
+        title: 'test2',
+        content: 'test2'
+      }
+    ];
   }
 
 }
